ed40b3273e92d690a5e3cfbffd1924693ea0da7e
kojima yosuke <yabbee0828@gmail.com>
2021-10-26 00:44:01 +0900

    Render my position

--- client/src/components/WorldMap.js
+++ client/src/components/WorldMap.js
@@ -6,24 +6,49 @@ import path from 'path';
 require('dotenv').config({ path: path.join(__dirname, '../', '../', '.env') });
 // mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
-// const getLngLat = async () => {
-//   navigator.geolocation.getCurrentPosition(
-//     (position) => {
-//       setLng(position.coords.longitude);
-//       setLat(position.coords.latitude);
-//     },
-//     (rejected) => {
-//       console.log(rejected);
-//     }
-//   );
-// };
-
 const WorldMap = () => {
   // hooks states
-  const [viewport, setViewport] = useState({ latitude: 47.040182, longitude: 17.071727, zoom: 4 });
+  const [viewport, setViewport] = useState({ latitude: 47.040182, longitude: 17.071727, zoom: 1 });
+  const [userPosition, setUserPosition] = useState({ lng: '', lat: '' });
   const [user, setUser] = useState({});
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // まずuserの情報を取ってきた後に、このlng, latのpropertyを加える。
+    const getLngLat = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserPosition({
+            ...userPosition,
+            lng: Number(position.coords.longitude.toFixed(1)),
+            lat: Number(position.coords.latitude.toFixed(1)),
+          });
+        },
+        (rejected) => {
+          console.log(rejected);
+        }
+      );
+    };
+    getLngLat();
+  }, []);
+
+  const markerRender = () => {
+    console.log(userPosition.lng, userPosition.lat);
+    if (userPosition.lng && userPosition.lat) {
+      return (
+        // ただ数値を入力するだけでは表示されないみたいだな。
+        <Marker
+          longitude={userPosition.lng}
+          latitude={userPosition.lat}
+          offsetLeft={-3.5 * viewport.zoom}
+          offsetTop={-7 * viewport.zoom}
+        >
+          You are here!!!!!!!!!!!!!!!!!!!!!
+        </Marker>
+      );
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>
@@ -35,7 +60,9 @@ const WorldMap = () => {
         mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
         onViewportChange={(viewport) => setViewport(viewport)}
         // onDblClick={currentUsername && handleAddClick}
-      />
+      >
+        {markerRender()}
+      </ReactMapGL>
     </div>
   );
 };
