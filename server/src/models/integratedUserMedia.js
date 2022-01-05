98ff5c5da2aebe3800e2a0ec3e573e811d3906e9
kojima yosuke <yabbee0828@gmail.com>
2022-01-05 14:13:27 +0900

    Commented out integratedusermedia model

--- server/src/models/integratedUserMedia.js
+++ server/src/models/integratedUserMedia.js
@@ -1,42 +1,42 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const integratedUserMediaSchema = new mongoose.Schema({
-  calledUserMedia: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'UserMedia',
-  },
-  calledUserScript: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'UserScript',
-  },
-  recievedUserMedia: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'UserMedia',
-  },
-  recievedUserScript: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'UserScript',
-  },
-  // comments: [], //ここはone to manyか。commentsっていうmodel作って、そっちでrefですればいいや。
-  // もしかしたら、ここはarrayに返るかも。groupチャット用の時も考えて。
-  // → chat 送信用のmodelも作らないといけない。one to manyでconversationかintegratedの方とrelationchipを実装する方向性になるだろう。
-  isPublic: Boolean,
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+// const integratedUserMediaSchema = new mongoose.Schema({
+//   calledUserMedia: {
+//     type: mongoose.Schema.ObjectId,
+//     ref: 'UserMedia',
+//   },
+//   calledUserScript: {
+//     type: mongoose.Schema.ObjectId,
+//     ref: 'UserScript',
+//   },
+//   recievedUserMedia: {
+//     type: mongoose.Schema.ObjectId,
+//     ref: 'UserMedia',
+//   },
+//   recievedUserScript: {
+//     type: mongoose.Schema.ObjectId,
+//     ref: 'UserScript',
+//   },
+//   // comments: [], //ここはone to manyか。commentsっていうmodel作って、そっちでrefですればいいや。
+//   // もしかしたら、ここはarrayに返るかも。groupチャット用の時も考えて。
+//   // → chat 送信用のmodelも作らないといけない。one to manyでconversationかintegratedの方とrelationchipを実装する方向性になるだろう。
+//   isPublic: Boolean,
+//   createdAt: {
+//     type: Date,
+//     default: Date.now(),
+//   },
+// });
 
-integratedUserMediaSchema.pre(/^find/, function (next) {
-  this.populate({
-    path: 'calledUserMedia',
-  });
-  this.populate({
-    path: 'recievedUserMedia',
-  });
+// integratedUserMediaSchema.pre(/^find/, function (next) {
+//   this.populate({
+//     path: 'calledUserMedia',
+//   });
+//   this.populate({
+//     path: 'recievedUserMedia',
+//   });
 
-  next();
-});
+//   next();
+// });
 
-const IntegratedUserMedia = mongoose.model('IntegratedUserMedia', integratedUserMediaSchema);
-export default IntegratedUserMedia;
+// const IntegratedUserMedia = mongoose.model('IntegratedUserMedia', integratedUserMediaSchema);
+// export default IntegratedUserMedia;
